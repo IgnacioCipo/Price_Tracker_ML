3ec46c58ab0b6dab06904bf2abbb83ce4af98934
IgnacioCipo <ignaciojcipolatti@gmail.com>
2023-07-11 16:19:18 -0300

    Getting the price of the product

--- main.py
+++ main.py
@@ -2,22 +2,22 @@ from selenium import webdriver
 import requests
 from bs4 import BeautifulSoup
 
-
-def getSource():
-    url = "https://articulo.mercadolibre.com.ar/MLA-721448642-raspberry-pi-3-b-plus-kit-64gb-a1-fuente-gab-disipa-e14-rs-_JM#reco_item_pos=0&reco_backend=machinalis-homes-pdp-boos&reco_backend_type=function&reco_client=home_navigation-recommendations&reco_id=6ac7c9a8-b64e-4cc2-a57f-5bc61c9ba317"
-    req = requests.get(url)
+# 
+def getSource(product_url):
+    req = requests.get(product_url)
     if req.status_code == 200:
-        print("Existing web page!")
+        print("Existing web page!\n")
     else:
         print("Web page doesn't exist!")
     return req
 
 def getPrice(response):
-    text_parsed = BeautifulSoup(response.text, "html.parser")
-    #search_results = text_parsed.find()
-    print(text_parsed)
-
-
+    text_parsed = BeautifulSoup(response.content, "html.parser")
+    price= text_parsed.find('span',class_ = "andes-money-amount__fraction").text
+    print("Price is: ", price)
+    return price
+    
 if __name__ == "__main__":
-    html_req = getSource()
+    url = "https://articulo.mercadolibre.com.ar/MLA-721448642-raspberry-pi-3-b-plus-kit-64gb-a1-fuente-gab-disipa-e14-rs-_JM#reco_item_pos=0&reco_backend=machinalis-homes-pdp-boos&reco_backend_type=function&reco_client=home_navigation-recommendations&reco_id=2eae1ea7-ea50-4c98-89bd-5504f54a934a"
+    html_req = getSource(url)
     getPrice(html_req)
